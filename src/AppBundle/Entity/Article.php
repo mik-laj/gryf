4ee9b46e192cb8176bce4b005a9d0412038f2bc3
Radziu <radek.k97@gmail.com>
2016-03-09 12:05:15 +0100

    cokolwiek

--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="articles")
  */
-class Articles
+class Article
 {
     /**
      * @ORM\Id
@@ -37,5 +37,84 @@ class Articles
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Article
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set menu
+     *
+     * @param \AppBundle\Entity\Submenu $menu
+     * @return Article
+     */
+    public function setMenu(\AppBundle\Entity\Submenu $menu = null)
+    {
+        $this->menu = $menu;
+
+        return $this;
+    }
+
+    /**
+     * Get menu
+     *
+     * @return \AppBundle\Entity\Submenu 
+     */
+    public function getMenu()
+    {
+        return $this->menu;
     }
 }
