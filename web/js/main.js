97faffbffedc5c588354054cd1ee65e4d5da6ae0
smerf <bart.zimo12@gmail.com>
2016-03-17 16:36:34 +0100

    HEY SPIN ME RIGHT NOW SPIN ME RIGHT NOW

---
+++ web/js/main.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//     $("button").toggle(
+//     	function() {
+//        $(this).addClass('facolor');
+//      }, function() {
+//        $(this).removeClass('facolor');
+//     });
+// });
